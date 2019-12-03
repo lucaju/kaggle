19051826284014283b8e55a68c7904a6da05e2f5
Luciano Frizzera <lucaju@me.com>
2019-12-02 19:16:32 -0500

    Fix scraper

--- src/scraper/datasets.js
+++ src/scraper/datasets.js
@@ -14,10 +14,10 @@ const collectDatasets = async page => {
 
 		//load page
 		await page.goto(url);
-		await page.waitFor('.datasets__list-wrapper');
+		await page.waitFor('.DatasetList_Wrapper-sc-c3jla7');
 
 		//get list of items
-		const items = await page.$$('.datasets__list-wrapper ul li');
+		const items = await page.$$('.DatasetList_Wrapper-sc-c3jla7 div ul li');
 		console.log(chalk.grey(`[${items.length}]`));
 		
 		let rank = 1; //ranking
@@ -27,10 +27,11 @@ const collectDatasets = async page => {
 			if (dataset) collection.push(dataset);
 
 			//scroll page
-			await page.waitFor(0.5 * 1000);
+			await page.waitFor(.5 * 1000);
 			page.evaluate( () => {
-				window.scrollBy(0, 89);
+				window.scrollBy(0, 88);
 			});
+			await page.waitFor(.5 * 1000);
 
 			rank += 1; //upadate ranking
 		}
@@ -49,19 +50,22 @@ const getDetails = async (item, rank, page) => {
 	try {
 
 		//------ 1. Main info
-		const title = await item.$eval('div > div > h3', content => content.innerHTML);
+		const title = await item.$eval('div:nth-child(1) div:nth-child(1) div:nth-child(1)', content => content.innerText);
 		console.log(`:: ${title}`);
 
-		const owner = await item.$eval('div > div > div > a > span ', content => content.innerText);
-		const endpoint = await item.$eval('a', content => content.getAttribute('href'));
-		const ownerEndpoint = await item.$eval('div > div > div > a', content => content.getAttribute('href'));
+		const owner = await item.$eval('div:nth-child(1) div:nth-child(1) span:nth-child(1) ', content => content.innerText);
+		
 
-		let filesize = await item.$eval('div > div > div:last-child > span:nth-child(2)', content => content.innerText);
+		const endpoint = await item.$eval('a', content => content.getAttribute('href'));
+		const ownerEndpoint = await item.$eval('div:nth-child(1) div:nth-child(1) span:nth-child(2) a', content => content.getAttribute('href'));
+		
+
+		let filesize = await item.$eval('div:nth-child(1) div:nth-child(1) span:nth-child(3) span:nth-child(2)', content => content.innerText);
 		filesize = filesize.split(' ');
 		
 		const sizeUnit = filesize[1];
 		let size = parseFloat(filesize[0]);
-
+		
 		//convert file size
 		if (sizeUnit.toLowerCase() == 'mb') {
 			size = size * 1024;
@@ -71,10 +75,10 @@ const getDetails = async (item, rank, page) => {
 			size = size * 1024 * 1024 * 1024;
 		}
 
-		let usability = await item.$eval('div > div > div:last-child > div > span', content => content.innerText);
+		let usability = await item.$eval('div:nth-child(1) div:nth-child(1) span:nth-child(3) div', content => content.innerText);
 		usability = parseFloat(usability.replace(/,/g, ''));
 
-		let files = await item.$eval('div > div > div:last-child > span:nth-child(4)', content => content.innerText);
+		let files = await item.$eval('div:nth-child(1) div:nth-child(1) span:nth-child(3) span:nth-child(4)', content => content.innerText);
 		let numFiles = files.match(/\d+/g);
 		numFiles = numFiles[0];
 		numFiles = parseFloat(numFiles);
@@ -87,7 +91,8 @@ const getDetails = async (item, rank, page) => {
 			fileTypes
 		};
 
-		let upvotes = await item.$eval('div > div > div:last-child > span:nth-child(5)', content => content.innerText);
+
+		let upvotes = await item.$eval('div:nth-child(1) div:nth-child(1) span:nth-child(3) span:nth-child(5)', content => content.innerText);
 		upvotes = upvotes.split(' ');
 		upvotes = upvotes[0];
 		upvotes = parseFloat(upvotes); // int????
@@ -98,11 +103,13 @@ const getDetails = async (item, rank, page) => {
 		// Mouse over to show "quick Look" button > click button to open modal
 		const itemBox = await item.boundingBox();
 		await page.mouse.move(itemBox.x + (itemBox.width/2), itemBox.y + (itemBox.height/2));
-		await page.waitFor(0.5 * 1000);
+		await page.waitFor(.5 * 1000);
 
 		//button
-		await page.click('button[data-testid]');
-		// await page.waitFor(0.5 * 1000);
+		// await page.click('button[data-testid]');
+		let button = await item.$eval('div.sc-eweMDZ button.MuiButton-contained', content => content.innerHTML);
+		await page.click('div.sc-eweMDZ button.MuiButton-contained');
+		await page.waitFor(.55 * 1000);
 		await page.waitFor('div .mdc-dialog__surface');
 
 		//modal
