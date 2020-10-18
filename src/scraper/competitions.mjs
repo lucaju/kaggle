1282d35549c8183ff10e8cf05f24defd2a7b3092
Luciano Frizzera <lucaju@gmail.com>
2020-10-18 18:23:20 -0400

    style: 💄 remove white space

--- src/scraper/competitions.mjs
+++ src/scraper/competitions.mjs
@@ -1,28 +1,27 @@
 import chalk from 'chalk';
 import { logError } from '../logs/datalog.mjs';
 
-const url ='https://www.kaggle.com/competitions';
-
+const url = 'https://www.kaggle.com/competitions';
 
 export const collectCompetitions = async (page) => {
-
 	const collection = [];
 
 	console.log('\n');
 	console.log(chalk.green.bold('Collecting Competitions'));
 
 	try {
-		
 		await page.goto(url);
 		await page.waitForSelector('#site-content');
 
 		//get list of items
-		const list = await page.$$('#site-content > div:nth-child(2) > div > div:nth-child(3) > div:nth-child(3) > ul > li:nth-child(odd)');
-		
+		const list = await page.$$(
+			'#site-content > div:nth-child(2) > div > div:nth-child(3) > div:nth-child(3) > ul > li:nth-child(odd)'
+		);
+
 		if (list.length === 0) {
 			const msg = {
 				title: 'Scraping',
-				message: 'Competition: List of items return 0'
+				message: 'Competition: List of items return 0',
 			};
 			console.log(msg);
 			logError(msg);
@@ -38,11 +37,10 @@ export const collectCompetitions = async (page) => {
 		}
 
 		return collection;
-
-	} catch(error) {
+	} catch (error) {
 		const msg = {
 			title: 'Scraping',
-			message: `Something is wrong with the scraping in Competions: ${error}`
+			message: `Something is wrong with the scraping in Competions: ${error}`,
 		};
 		console.log(msg);
 		logError(msg);
@@ -51,24 +49,32 @@ export const collectCompetitions = async (page) => {
 };
 
 const getDetails = async (item) => {
-
 	try {
-
-		const title = await item.$eval('a > span:nth-child(2) > div:first-child', content => content.innerText);
+		const title = await item.$eval(
+			'a > span:nth-child(2) > div:first-child',
+			(content) => content.innerText
+		);
 		console.log(`:: ${title}`);
 
-		const endpoint = await item.$eval('a', content => content.getAttribute('href'));
-		const shortDescription = await item.$eval('a > span:nth-child(2) > span', content => content.innerText);
-		
+		const endpoint = await item.$eval('a', (content) => content.getAttribute('href'));
+		const shortDescription = await item.$eval(
+			'a > span:nth-child(2) > span',
+			(content) => content.innerText
+		);
+
 		const metadata = {};
-		let meta = await item.$eval('a > span:nth-child(2) > span:nth-child(3)', content => content.innerText);
+		let meta = await item.$eval(
+			'a > span:nth-child(2) > span:nth-child(3)',
+			(content) => content.innerText
+		);
+
 		if (meta) {
 			meta = meta.trim();
 			meta = meta.split('•');
-			
+
 			metadata.category = meta[0].trim();
 			metadata.relativeDeadline = meta[1].trim();
-			
+
 			if (meta.length > 2) {
 				let subCatPos = 2;
 				let teamPos = 3;
@@ -80,9 +86,8 @@ const getDetails = async (item) => {
 			}
 		}
 
-		const prize = await item.$eval('div:nth-child(2)', content => content.innerText);
+		const prize = await item.$eval('div:nth-child(2)', (content) => content.innerText);
 
-		//return object 
 		return {
 			title,
 			endpoint,
@@ -91,13 +96,12 @@ const getDetails = async (item) => {
 			relativeDeadline: metadata.relativeDeadline,
 			subCategory: metadata.subCategory,
 			teams: metadata.teams,
-			prize
+			prize,
 		};
-
 	} catch (error) {
 		const msg = {
 			title: 'Scraping',
-			message: `Competion: Something is wrong with one of the competitions: ${error}`
+			message: `Competion: Something is wrong with one of the competitions: ${error}`,
 		};
 		console.log(msg);
 		logError(msg);
