3571a4ece1013438619951f63f146bd41ceaabac
Luciano Frizzera <lucaju@gmail.com>
2020-10-24 17:20:57 -0400

    fix: 🐛 add user scrape to the main code

--- src/scraper/scraper.mjs
+++ src/scraper/scraper.mjs
@@ -1,19 +1,19 @@
 import { collectCompetitions } from './competitions.mjs';
 import { collectDatasets } from './datasets.mjs';
-// import { collectUsers } from './users.mjs';
+import { collectUsers } from './users.mjs';
 
 export const scraper = async (target, page) => {
 	switch (target) {
 	case 'competitions':
 		return await collectCompetitions(page);
 	case 'datasets':
-		await collectDatasets(page);
-	// case 'users':
-	// 	return  await collectUsers(page);
+		return await collectDatasets(page);
+	case 'users':
+		return  await collectUsers(page);
 	}
 };
 
-export const limitScrollTo = 75; //null
+export const limitScrollTo = 50; //null
 
 export default {
 	scraper,
