d25795bd2c759054756e7b7e12e97c21fe3f5200
Luciano Frizzera <lucaju@gmail.com>
2020-12-07 11:29:44 -0500

    fix: 🐛 fix filter

--- src/scraper/internal/config.mjs
+++ src/scraper/internal/config.mjs
@@ -11,7 +11,7 @@ export const config = {
 		},
 	],
 	filter: {
-		details: { $exists: true }
+		details: { $exists: false }
 	},
 	limit: 100,
 	puppeteer: { headless: false },
@@ -27,7 +27,7 @@ export const config = {
 		retryLimit: 1,
 		retryDelay: 10000,
 		sameDomainDelay: 4000,
-		timeout: 300000,
+		timeout: 600000,
 		monitor: true,
 		workerCreationDelay: 40,
 	}
